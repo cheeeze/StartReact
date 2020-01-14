486ca35894e5ac5d978369e150018d33e5ca791c
cheeeze <hj951219@gmail.com>
2020-01-14 10:22:32 +0900

    App.js에서 function type을 class type으로 변경

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//기본으로 생성된 function type
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +22,30 @@ function App() {
       </header>
     </div>
   );
+}*/
+
+//class type
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
