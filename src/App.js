fbaa43279bcd6f38cd44a7ef1e532b8dde3b22cb
cheeeze <hj951219@gmail.com>
2020-01-14 10:29:48 +0900

    기본적인 리액트 코드작성 연습

--- src/App.js
+++ src/App.js
@@ -28,21 +28,9 @@ import './App.css';
 class App extends Component{
   render(){
     return (
+      //반드시 하나의 태그 안쪽에 나머지 태그들이 있어야 함(가장 바깥쪽에는 태그 하나만)
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        Hello, React!!!
       </div>
     );
   }
