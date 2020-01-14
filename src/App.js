604cdeeb5b48c8878f1a16df97c2b6947975749c
cheeeze <hj951219@gmail.com>
2020-01-14 16:45:25 +0900

    Craete 시에 form에 정보를 받고 onSubmit활용하여 Content 변경 구현

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ import './App.css';
 class App extends Component{
   constructor(props){
     super(props);
+    this.max_content_id = 3; //ui에 영향을 주지 않기 때문에 state값으로 안함
     this.state = {
-      mode:'read',
+      mode:'create',
       selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'Welcome',desc:'Hello, React!!'},
@@ -45,7 +46,20 @@ class App extends Component{
       }
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     }else if(this.state.mode === 'create'){
-      _article = <CreateContent></CreateContent>
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        //add content to this.state.contents
+        this.max_content_id = this.max_content_id+1;
+        // this.state.contents.push(
+        //   {id:this.max_content_id, title:_title, desc: _desc}
+        // );
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc: _desc}
+        )
+        this.setState({
+          contents:_contents
+        });
+        console.log(_title, _desc);
+      }.bind(this)}></CreateContent>
     }
     return (
       //반드시 하나의 태그 안쪽에 나머지 태그들이 있어야 함(가장 바깥쪽에는 태그 하나만)
